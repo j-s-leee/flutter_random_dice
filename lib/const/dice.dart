9c7ba5473438e34d2a19c06c66a3bbae5df7170f
ljs <ljs@b2en.com>
2024-07-21 18:13:58 +0900

    first commit

---
+++ lib/const/dice.dart
@@ -0,0 +1,11 @@
+import 'package:dice_icons/dice_icons.dart';
+
+const dice = [
+  DiceIcons.dice0,
+  DiceIcons.dice1,
+  DiceIcons.dice2,
+  DiceIcons.dice3,
+  DiceIcons.dice4,
+  DiceIcons.dice5,
+  DiceIcons.dice6
+];
